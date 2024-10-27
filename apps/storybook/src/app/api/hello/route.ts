a76f213042cdbd70d21572ab9f6ee3ed87b7f42a
rahulapgm <rahulpgm@gmail.com>
2024-10-28 03:06:51 +0530

    init

--- apps/storybook/src/app/api/hello/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest } from "next";
-import { NextResponse } from "next/server";
-
-export async function GET(request: NextApiRequest) {
-  return new NextResponse('Hello, from API!');
-}
